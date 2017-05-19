31e237160ba64f0071a44876df70646e065d8cd1
patricxu <qxu000@163.com>
2017-05-19 11:24:43 +0800

    python_lib/

--- c/python_lib/call.py
+++ c/python_lib/call.py
@@ -6,12 +6,21 @@ from ctypes import *
 #python中结构体定义  
 class Block(Structure):  
     _fields_ = [("name", c_char * 20), ("age", c_int)]  
+
+class Block2(Structure):
+    _fields_ = [("ptr", POINTER(c_float)), ("len", c_int)]
   
 if __name__ == "__main__":  
     lib = cdll.LoadLibrary("./libtest.so")  
     lib.test.restype = POINTER(Block)  
+    lib.test2.restype = POINTER(Block2)
     p = lib.test()  
   
     print ("%s: %d" %(p.contents.name, p.contents.age)) 
+
+    p = lib.test2()
+    print("=================================")
+    print(float(p.contents.ptr[0]))
+    print(p.contents.ptr[1])
 
 #使用byref(structure_obj)获取指向obj的指针
--- c/python_lib/test.cpp
+++ c/python_lib/test.cpp
@@ -18,4 +18,25 @@ extern "C" StructPointer test()    // 返回结构体指针
     p->age = 20;  
       
     return p;   
-}  
+}
+
+typedef struct StructPointerTest2
+{
+    float* ptr;
+    int len;
+}StructPointerTest2, *StructPointer2;
+
+extern "C" StructPointer2 test2()    // 返回结构体指针  
+{
+    StructPointer2 p = (StructPointer2)malloc(sizeof(StructPointerTest2));
+    p->ptr = (float*)malloc(16);
+    memset(p->ptr, 0, 16);
+    p->ptr[0] = 1.5;
+    p->ptr[1] = 2;
+//    memset(p->ptr, 0xff, 16);
+    p->len = 2;
+
+    return p;
+}
+  
+  
